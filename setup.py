e132999b9299e8d718aac8971725fb5ed312318d
Casper Kaandorp <c.s.kaandorp@uu.nl>
2020-12-01 12:09:11 +0100

    WindowsPath

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='anonymoUUs',
-    version='0.0.5',
+    version='0.0.6',
     description='A tool to substitue patterns/names in a file tree',
     long_description = long_description,
     long_description_content_type = "text/markdown",
