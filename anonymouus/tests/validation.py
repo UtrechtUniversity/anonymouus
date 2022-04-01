cb9d7c9eb88786d736bac61f77d49996c930fcb2
MartineDeVos <mgdevos@gmail.com>
2022-04-01 22:04:58 +0200

    WIP: add compute freqs, stats and metrics

--- anonymouus/tests/validation.py
+++ anonymouus/tests/validation.py
@@ -2,7 +2,7 @@
 
 Evaluate the performance of the deidentification by comparing it to a labeled
 ground truth. To run the validation make sure you have these data present:
-* anonymized files 
+* anonymized files
 * key file
 * manually labeled ground truth (created with Label Studio)
 Examples can be found in the test_data in this folder
@@ -23,31 +23,44 @@ import re
 
 class Validation:
     """ Validate deidentification by comparing to a labeled ground truth """
-    
+
     def __init__(self, anymdir, gtfile, keyfile):
-        self.gt_labels = self.read_gt(gtfile)
+        self.gt = self.read_gt(gtfile)
+        self.labels = self.gt_labels()
+        self.raw_text = self.gt['task']['data']['my_text']
         self.keys = self.read_keys(keyfile)
         self.anym_text = self.read_anym(anymdir)
 
-    def read_gt(self, file):
+    def read_gt(self, file: Path) -> dict:
         """ Read manually labeled ground truth file """
 
         with open(file, 'r') as f:
-            gt_labels = json.loads(f.read())
+            gt = json.loads(f.read())
+
+        return gt
+
+    def gt_labels(self) -> dict:
+        """Extract PII labels from ground truth"""
+
+        gt_labels = {
+                    i['value']['text'].strip():
+                    {'label': i['value']['labels'][0]}
+                    for i in self.gt['result']
+                    }
 
         return gt_labels
 
-    def read_keys(self, file):
+    def read_keys(self, file: Path) -> dict:
         """ Read file with PII keys and substitutes"""
         with file.open() as f:
             reader = csv.DictReader(f)
             keys = {row['names']: row['subt'] for row in reader}
 
-        return keys    
+        return keys
 
-    def read_anym(self, dir):
+    def read_anym(self, dir: Path) -> str:
         """ Read all anonymized files"""
-        
+
         files = list(dir.glob('*.txt'))
 
         for file in files:
@@ -55,6 +68,91 @@ class Validation:
                 anym_text = f.read()
 
         return anym_text
+
+    def find_key(self, name: str) -> str:
+        """Find key(substitute) for a given name"""
+        try:
+            subt = self.keys[name.strip()]
+        except KeyError:
+            print(f'No hash for {name}')
+            subt = '__NOHASH__'
+
+        return subt
+
+    def count_subt(self, item: str) -> int:
+        """Count frequency of substitutes in anonymized text
+           for a given PII item"""
+
+        if self.labels[item]['label'] == 'EMAIL':
+            subt = '__email'
+        elif self.labels[item]['label'] == 'LOC':
+            subt = '__location'
+        else:  # item is a name
+            subt = self.find_key(item)
+
+        freq_subt = len(re.findall(subt, self.anym_text))
+
+        return freq_subt
+
+    def compute_freqs(self, item: str) -> dict:
+        """Count frequencies in raw and anonymized text
+           per PII item and its substitute"""
+
+        freqs = {}
+        # count freq of label in ground truth
+        freq_gt = len(re.findall(item, self.raw_text))
+        freqs['freq_gt'] = freq_gt
+
+        # count freq of label in anonymized text
+        freq_anym = len(re.findall(item, self.anym_text))
+        freqs['freq_anym'] = freq_anym
+
+        # count freq of substitute in anonymized text
+        freq_subt = self.count_subt(item)
+        freqs['freq_subt'] = freq_subt
+
+        return freqs
+
+    def compute_stats(self, item: str) -> dict:
+        """ Compute true positives, false positives, false negatives
+            per PII item """
+
+        stats = {}
+        TP = self.labels[item]['freq_gt'] - self.labels[item]['freq_anym']
+        if TP < 0:
+            TP = 0
+        stats['TP'] = TP
+
+        if self.labels[item]['freq_subt'] > TP:
+            self.labels[item]['freq_subt'] = TP
+        FP = self.labels[item]['freq_subt'] - TP
+        stats['FP'] = FP
+
+        FN = self.labels[item]['freq_anym']
+        if FN > self.labels[item]['freq_gt']:
+            FN = self.labels[item]['freq_gt']
+        stats['FN'] = FN
+
+        return stats
+
+    def validate(self) -> dict:
+        """Validate deidentification"""
+
+        for item in self.labels.keys():
+
+            freqs = self.compute_freqs(item)
+            self.labels[item].update(freqs)
+
+        # summarize frequencies per label category
+        labels_df = pd.DataFrame(self.labels).transpose()
+        grp_df = labels_df.groupby('label').sum()
+        grp_dict = grp_df.transpose().to_dict()
+
+        for label in grp_dict.keys():
+            metrics = self.compute_metrics(label)
+            grp_dict[label].update(metrics)
+
+        return grp_dict
 
 
 def main():
@@ -65,11 +163,13 @@ def main():
                         default=test_data/'anonymized')
     parser.add_argument("--gtfile", "-g", help="Labeled groundtruth file",
                         default=test_data/'labeled.json')
-    parser.add_argument("--keyfile", "-k", help="Keys file", 
-                        default=test_data/'keys.csv')                                        
+    parser.add_argument("--keyfile", "-k", help="Keys file",
+                        default=test_data/'keys.csv')
     args = parser.parse_args()
 
     validator = Validation(args.anymdir, args.gtfile, args.keyfile)
+    metrics = validator.validate()
+    print(metrics)
 
 
 if __name__ == '__main__':
